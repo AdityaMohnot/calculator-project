8e7af8037d6e5d004e0712f8b0de37ca50f387ed
AdityaMohnot <94472910+AdityaMohnot@users.noreply.github.com>
2023-01-02 18:28:57 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,12 +1,13 @@
 function func(b){
     if (b == "clear"){
         document.getElementById("feild").innerHTML = " "
+    } else if (b == "back"){
+        var a = String(document.getElementById("feild").textContent)
+        a = a.slice(0, a.length-1)
+        document.getElementById("feild").innerHTML = a
     } else if (b != "e"){
         document.getElementById("feild").innerHTML += b
     } else if (b == "e"){
         document.getElementById("feild").innerHTML = Function("return " + document.getElementById("feild").textContent)()
-    } else if (b == "back"){
-        var a = String(document.getElementById("feild").textContent)
-        a = a
     }
 }
